e8f06aab4379065bdf2bd0952e3a723005de0564
funao <funa2754work@gmail.com>
2025-02-23 23:44:44 +0900

    小数点の修正

--- transport.py
+++ transport.py
@@ -1,15 +1,16 @@
 #必要な家賃と、居住坪数を入力すれば、補助の金額を計算してcsvに出力する。
 #5500円以下なら0円
+#Windowsのデスクトップから実行する場合は、プロパティリンク先(T)に Python のインタープリタファイルとプログラムファイルのパスを入力。
 from decimal import Decimal, ROUND_HALF_UP
 import csv
 import os
 
-print("部屋の名前、URL（あれば）、家賃、居住坪数を入力してください。")
+print("部屋の名前、URL、家賃、居住坪数を入力してください。")
 room_name = input("部屋の名前:")
 url = input("URL:")
-rent = float(input("家賃(円):"))   
+rent = float(input("家賃(円) ※共益費、管理費、駐車場は含まない:"))   
 area = float(input("居住面積(m^2):"))
-area_tubo = float(Decimal(area / 3.3).quantize(Decimal("0.01"), rounding=ROUND_HALF_UP))
+area_tubo = float(Decimal(area / 3.3).quantize(Decimal("0.1"), rounding=ROUND_HALF_UP))
 area_jou = Decimal(area /1.548).quantize(Decimal("0.01"), rounding=ROUND_HALF_UP)
 default_subsidy = 8500
 #2畳を1坪、3.3m^2を1坪とし、少数第二位を四捨五入する
@@ -18,7 +19,6 @@ if rent >= 100000:
     rent = 100000
 
 subsidy = (rent -8500 -(1800*area_tubo))*0.325
-#subsidy = (Decimal(subsidy) / Decimal("100")).quantize(Decimal("1"), rounding=ROUND_HALF_UP) * Decimal("100")
 subsidy = int(subsidy / 1000) * 1000
 rent = int(rent)
 if subsidy < 5500:
